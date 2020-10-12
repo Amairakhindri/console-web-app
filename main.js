1d33c563fda3d3a4304dd2236262f4a247475d6b
Amairakhindri <72617628+Amairakhindri@users.noreply.github.com>
2020-10-12 19:53:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("my first webpage");
+var firstname="Amaira"
+var secondname="Khindri"
+var fullname= firstname.concat("secondname")
+console.log(fullname)
+var a=10
+var b= 5
+var c= a+b
+console.log(c)
+var d=a-b
+console.log(d)
